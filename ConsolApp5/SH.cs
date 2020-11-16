6cc0d177e83b11d8f501e3ddc2ea4b433a37717b
leeseonghyuck <zzangjb2@gmail.com>
2020-11-16 17:42:16 +0900

    내가 이성혁

---
+++ ConsolApp5/SH.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarCraft
+{
+    public class SH
+    {
+        string Name = "성혁";
+        int king = 3;
+    }
+}
