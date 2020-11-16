b659c5be9acfbce62a59d826fb05573ee65f2f80
DAE-GYUN <f45ee@naver.com>
2020-11-16 17:26:10 +0900

    .

--- ConsolApp5/Program.cs
+++ ConsolApp5/Program.cs
@@ -8,6 +8,7 @@ namespace StarCraft
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("민균씨 바보");
+            Console.WriteLine("클래스 만들어요");
         }
     }
 }
